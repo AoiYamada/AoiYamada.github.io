df60b2318c0fe8e46142d98432174dd8cb80afec
AoiYamada <knowledgekong@gmail.com>
2019-07-26 09:37:03 +0800

    theme style fix & sidebar style
    z-index of sidebar fix
    
    
    fix menu click toggle sidebar
    
    
    twitter share fix
    
    
    landscape style fix
    landscape style fix
    fix sidebar btn position
    
    
    Update index.html

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,11 @@ const app_headers = document.querySelectorAll('.app-header');
 app_headers.forEach(function (app_header) {
   app_header.addEventListener('click', function () {
     app_header.classList.toggle("app-header-show");
-  });
+  }, false);
+});
+const sidebar_menus = document.querySelectorAll('.sidebar-menu');
+sidebar_menus.forEach(function (sidebar_menu) {
+  sidebar_menu.addEventListener('click', function (e) {
+    e.stopPropagation()
+  }, false);
 });
